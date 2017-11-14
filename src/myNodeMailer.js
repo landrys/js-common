fa7b504a7cf9bea46c9b2dedb9e6633c109d7c00
Leon Landry <landryseleven@gmail.com>
2017-11-14 14:32:11 -0500

    Removed privateStuff.

--- src/myNodeMailer.js
+++ src/myNodeMailer.js
@@ -1,13 +1,12 @@
 /*jslint node: true */
 "use strict";
 var nodemailer = require('nodemailer');
-var privateStuff = require('./private');
+//var privateStuff = require('./private');
 
-var transporter = nodemailer.createTransport({
+mudule.exports.transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true,
-    auth: privateStuff.auth
+    secure: true
 });
 
 module.exports.mailIt = function (to, subject, body) {
@@ -18,7 +17,7 @@ module.exports.mailIt = function (to, subject, body) {
         text: body
     };
 
-    transporter.sendMail(mailOptions, function (error, info) {
+    exports.transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log(error);
         } else {
